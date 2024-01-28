cadd8e6d4babec294991380ade5c0b509cfdaeb7
Alphina18 <aimeepruis@outlook.com>
2024-01-28 18:19:11 +0100

    update to website, conclusion,

--- app.py
+++ app.py
@@ -2,21 +2,18 @@ from flask import Flask, render_template, request  # Importing Flask, render_tem
 
 app = Flask(__name__)  # getting the import name
 
+
 @app.route('/')
 def root():
     active_page = 'root'
     return render_template('index.html', active_page=active_page)
+
 
 @app.route('/peanut_immuno')
 def peanut_immuno():
     active_page = 'peanut_immuno'
     return render_template('peanut_immuno.html', active_page=active_page)
 
-@app.route('/conclusion')  # Route for the conclusion page
-def conclusion():
-    active_page = 'conclusion'
-    # Add any specific logic for the conclusion page if needed
-    return render_template('conclusion.html', active_page=active_page)
 
 @app.route('/tips')  # Route for the tips page
 def tips():
@@ -30,11 +27,13 @@ def research():
     # Add any specific logic for the research page if needed
     return render_template('research.html', active_page=active_page)
 
+
 @app.route('/sources')  # Route for the sources page
 def sources():
     active_page = 'sources'
     # Add any specific logic for the sources page if needed
     return render_template('sources.html', active_page=active_page)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
